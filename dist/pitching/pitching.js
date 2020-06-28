f5675be41e7b0eb5a78200554b2807b44bbdd8e1
el-ethan <elethan@gmail.com>
2020-06-28 14:49:03 -0500

    compile with k9

--- dist/pitching/pitching.js
+++ dist/pitching/pitching.js
@@ -4,7 +4,12 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.whip = whip;
+exports.k9 = k9;
 
 function whip(walks, hitsAllowed, inningsPitched) {
   return (walks + hitsAllowed) / inningsPitched;
+}
+
+function k9(strikeouts, inningsPitched) {
+  return strikeouts * 9 / inningsPitched;
 }
